f8fbbf37781d8623116f851a4670191221c53afe
HP-Retro <28hpoirier@aos92.org>
2025-11-18 12:53:04 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -146,31 +146,6 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                 `, obsidian, 80, 0)
         })
         pause(100)
-        if (info.score() >= 500) {
-            if (LostSoul == true) {
-                timer.after(500, function () {
-                    chargeblast = sprites.createProjectileFromSprite(img`
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . f f . . . . . f f f . . . . . 
-                        f f f . f f f f f f f f f a a . 
-                        f f f f f f f f f f f f f f a a 
-                        a f f f f f f f f a f f f f f a 
-                        a a a a a a a a a a a a a a a a 
-                        a a a a a a a a a a a a a a a a 
-                        a f f f f f f f f a f f f f f a 
-                        f f f f f f f f f f f f f f a a 
-                        f f f . f f f f f f f f f a a . 
-                        . f f . . . . . f f f . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        `, obsidian, 80, 0)
-                })
-                pause(100)
-            }
-        }
         if (info.score() >= 1500) {
             if (LostSoul == true) {
                 timer.after(500, function () {
@@ -193,29 +168,6 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
                         . . . . . . . . . . . . . . . . 
                         `, obsidian, 80, 0)
                 })
-                pause(100)
-            }
-        }
-        if (info.score() >= 3000) {
-            if (LostSoul == true) {
-                chargeblast = sprites.createProjectileFromSprite(img`
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . f f . . . . . f f f . . . . . 
-                    f f f . f f f f f f f f f a a . 
-                    f f f f f f f f f f f f f f a a 
-                    a f f f f f f f f a f f f f f a 
-                    a a a a a a a a a a a a a a a a 
-                    a a a a a a a a a a a a a a a a 
-                    a f f f f f f f f a f f f f f a 
-                    f f f f f f f f f f f f f f a a 
-                    f f f . f f f f f f f f f a a . 
-                    . f f . . . . . f f f . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    `, obsidian, 80, 0)
                 pause(100)
             }
         }
@@ -1139,7 +1091,6 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile23`, function (sprite, 
     tiles.placeOnTile(obsidian, tiles.getTileLocation(9, 3))
 })
 let helioite: Sprite = null
-let diamond: Sprite = null
 let ruby: Sprite = null
 let opal: Sprite = null
 let you: Sprite = null
@@ -1293,14 +1244,51 @@ obsidian = sprites.create(img`
     . . . . f f b f b f b f . . . . 
     `, SpriteKind.Player)
 scene.cameraFollowSprite(obsidian)
+let mySprite = sprites.create(img`
+    . . . . . . e e e e . . . . . . 
+    . . . . e e e 4 4 e e f . . . . 
+    . . . f e 4 4 4 4 4 4 e f . . . 
+    . . f e 4 e e e e e e 4 e f . . 
+    . . f e e 5 4 4 4 4 5 e e f . . 
+    . . f e 4 e 5 e e 5 e 4 e f . . 
+    . . f f e f f 5 5 f f e f f . . 
+    . f f e f 4 4 f f 4 4 f e f f . 
+    . f e e f f 2 f f 2 f f e e f . 
+    . . f e e f f f f f f e e f . . 
+    . . . f e 4 f f f f 4 e f . . . 
+    e e e e 4 e 4 e e 4 e 4 e e e e 
+    e 2 4 2 f 4 e e e e 4 f 2 4 2 e 
+    e 2 4 f 4 f e e e e f 4 f 4 2 e 
+    2 2 2 . . 4 f e e f 4 . . 2 2 2 
+    . 2 . . . f f . . f f . . . 2 . 
+    `, SpriteKind.miniboss)
+let myEnemy = sprites.create(img`
+    . . . . . . d d d d . . . . . . 
+    . . . . d d d b b d d f . . . . 
+    . . . f d b b b b b b d f . . . 
+    . . f d b d d d d d d b d f . . 
+    . . f d d f b b b b f d d f . . 
+    . . f d b d f d d f d b d f . . 
+    . . f f d 1 1 f f 1 1 d f f . . 
+    . f f d 1 6 6 1 1 6 6 1 d f f . 
+    . f d d 1 1 b 1 1 b 1 1 d d f . 
+    . . f d d 1 1 1 1 1 1 d d f . . 
+    . . . f d b 1 1 1 1 b d f . . . 
+    d d d d b d b d d b d b d d d d 
+    d 9 b 9 f b d d d d b f 9 b 9 d 
+    d 9 b f b f d d d d f b f b 9 d 
+    9 9 9 . . b f d d f b . . 9 9 9 
+    . 9 . . . f f . . f f . . . 9 . 
+    `, SpriteKind.miniboss)
 bleh = false
 isInvincible = false
 shards = true
+tiles.placeOnTile(myEnemy, tiles.getTileLocation(1, 129))
 tiles.placeOnTile(obsidian, tiles.getTileLocation(127, 127))
+tiles.placeOnTile(mySprite, tiles.getTileLocation(127, 4))
 controller.moveSprite(obsidian, 85, 85)
 LostSoul = true
 info.setLife(50)
-info.setScore(3000)
 game.onUpdateInterval(15000, function () {
     opal = sprites.create(img`
         ........................
@@ -1330,69 +1318,9 @@ game.onUpdateInterval(15000, function () {
         `, SpriteKind.t2enemy)
     sprites.setDataNumber(opal, "HP", 75)
     tiles.placeOnRandomTile(opal, assets.tile`myTile8`)
-    opal.follow(obsidian, 80)
-})
-game.onUpdateInterval(15000, function () {
-    ruby = sprites.create(img`
-        ........................
-        ........................
-        ........................
-        ........................
-        ..........ffff..........
-        ........ff1111ff........
-        .......fb111111bf.......
-        .......f11111111f.......
-        ......fd11111111df......
-        ......fd11111111df......
-        ......fddd1111dddf......
-        ......fbdbfddfbdbf......
-        ......fcdcf11fcdcf......
-        .......fb111111bf.......
-        ......fffcdb1bdffff.....
-        ....fc111cbfbfc111cf....
-        ....f1b1b1ffff1b1b1f....
-        ....fbfbffffffbfbfbf....
-        .........ffffff.........
-        ...........fff..........
-        ........................
-        ........................
-        ........................
-        ........................
-        `, SpriteKind.Enemy)
-    sprites.setDataNumber(ruby, "HP", 50)
-    tiles.placeOnRandomTile(ruby, assets.tile`myTile8`)
-    ruby.follow(obsidian, 70)
-})
-game.onUpdateInterval(600000, function () {
-    diamond = sprites.create(img`
-        ...........ff...........
-        ..........faaf..........
-        .....f...faaaaf...f.....
-        ....faf...faaf...faf....
-        ....faaf..ffff..faaf....
-        .....faaffffffffaaf.....
-        ......ffffffffffff......
-        ......ffffffffffff......
-        ......ffffffffffff......
-        ....ffffffffffffffff....
-        ...faafffaaffaafffaaf...
-        ..faaffffaaffaaffffaaf..
-        ..faf.fffaaffaafff.faf..
-        ...f..ffffffffffff..f...
-        .....ffffaffffaffff.....
-        ....fffffaaffaafffff....
-        ....ffffffaaaaffffff....
-        ....fffffffaafffffff....
-        ........ffffffff........
-        .........ffffff.........
-        ........................
-        ........................
-        ........................
-        ........................
-        `, SpriteKind.miniboss)
-    sprites.setDataNumber(diamond, "HP", 500)
-    tiles.placeOnRandomTile(diamond, assets.tile`myTile28`)
-    diamond.follow(obsidian, 90)
+    timer.after(20000, function () {
+        sprites.destroy(opal)
+    })
 })
 forever(function () {
     if (info.score() >= 1000) {
@@ -1492,6 +1420,83 @@ forever(function () {
         pause(10000)
     }
 })
+forever(function () {
+    if (!(spriteutils.isDestroyed(ruby))) {
+        if (spriteutils.distanceBetween(obsidian, ruby) > 125) {
+            ruby.follow(null)
+        } else {
+            ruby.follow(obsidian, 80)
+        }
+    }
+    pause(100)
+    if (!(spriteutils.isDestroyed(ruby))) {
+        if (spriteutils.distanceBetween(obsidian, ruby) > 125) {
+            ruby.follow(null)
+        } else {
+            ruby.follow(obsidian, 80)
+        }
+    }
+})
+forever(function () {
+    if (!(spriteutils.isDestroyed(helioite))) {
+        if (spriteutils.distanceBetween(obsidian, opal) > 125) {
+            helioite.follow(null)
+        } else {
+            helioite.follow(obsidian, 80)
+        }
+    }
+    pause(100)
+    if (!(spriteutils.isDestroyed(helioite))) {
+        if (spriteutils.distanceBetween(obsidian, opal) > 125) {
+            helioite.follow(null)
+        } else {
+            helioite.follow(obsidian, 80)
+        }
+    }
+})
+forever(function () {
+    if (!(spriteutils.isDestroyed(mySprite))) {
+        if (spriteutils.distanceBetween(obsidian, mySprite) > 300) {
+            mySprite.follow(null)
+        } else {
+            mySprite.follow(obsidian, 100)
+        }
+    }
+    pause(100)
+    if (!(spriteutils.isDestroyed(mySprite))) {
+        if (spriteutils.distanceBetween(obsidian, mySprite) > 300) {
+            mySprite.follow(null)
+        } else {
+            mySprite.follow(obsidian, 100)
+        }
+    }
+})
+forever(function () {
+    if (!(spriteutils.isDestroyed(opal))) {
+        if (spriteutils.distanceBetween(obsidian, opal) > 125) {
+            opal.follow(null)
+        } else {
+            opal.follow(obsidian, 80)
+        }
+    }
+})
+forever(function () {
+    if (!(spriteutils.isDestroyed(myEnemy))) {
+        if (spriteutils.distanceBetween(obsidian, myEnemy) > 300) {
+            myEnemy.follow(null)
+        } else {
+            myEnemy.follow(obsidian, 100)
+        }
+    }
+    pause(100)
+    if (!(spriteutils.isDestroyed(myEnemy))) {
+        if (spriteutils.distanceBetween(obsidian, myEnemy) > 300) {
+            myEnemy.follow(null)
+        } else {
+            myEnemy.follow(obsidian, 100)
+        }
+    }
+})
 game.onUpdateInterval(20000, function () {
     helioite = sprites.create(img`
         ........................
@@ -1521,5 +1526,40 @@ game.onUpdateInterval(20000, function () {
         `, SpriteKind.t3enemy)
     sprites.setDataNumber(helioite, "HP", 100)
     tiles.placeOnRandomTile(helioite, assets.tile`myTile8`)
-    helioite.follow(obsidian, 80)
+    timer.after(20000, function () {
+        sprites.destroy(helioite)
+    })
+})
+game.onUpdateInterval(10000, function () {
+    ruby = sprites.create(img`
+        ........................
+        ........................
+        ........................
+        ........................
+        ..........ffff..........
+        ........ff1111ff........
+        .......fb111111bf.......
+        .......f11111111f.......
+        ......fd11111111df......
+        ......fd11111111df......
+        ......fddd1111dddf......
+        ......fbdbfddfbdbf......
+        ......fcdcf11fcdcf......
+        .......fb111111bf.......
+        ......fffcdb1bdffff.....
+        ....fc111cbfbfc111cf....
+        ....f1b1b1ffff1b1b1f....
+        ....fbfbffffffbfbfbf....
+        .........ffffff.........
+        ...........fff..........
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Enemy)
+    sprites.setDataNumber(ruby, "HP", 50)
+    tiles.placeOnRandomTile(ruby, assets.tile`myTile8`)
+    timer.after(20000, function () {
+        sprites.destroy(ruby)
+    })
 })
